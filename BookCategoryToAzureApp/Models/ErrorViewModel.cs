a1e3716f21e55978b548045c9b76e6d60425efc8
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-02-26 14:24:49 +0100

    Add project files.

---
+++ BookCategoryToAzureApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookCategoryToAzureApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
