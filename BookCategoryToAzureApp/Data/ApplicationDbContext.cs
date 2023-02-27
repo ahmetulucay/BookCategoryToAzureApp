ccdd6a9b706f3b4cb9cf77ebb731e3e58e29c1f4
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-02-27 13:55:51 +0100

    Context&Migrations

---
+++ BookCategoryToAzureApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BookCategoryToAzureApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookCategoryToAzureApp.Data;
+
+public class ApplicationDbContext : DbContext
+{
+	//public readonly DbContextOptions _options;
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+		//_options = options;
+	}
+	public DbSet<Category> Categories { get; set; }
+}
