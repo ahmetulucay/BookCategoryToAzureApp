33c5736f54988ef4d7896913629faadb9de335e2
Ahmet U <57094137+GitAhmet@users.noreply.github.com>
2023-03-01 00:22:15 +0100

    Validation&Css&Create

---
+++ BookCategoryToAzureApp/Controllers/CategoryController.cs
@@ -0,0 +1,45 @@
+﻿using BookCategoryToAzureApp.Data;
+using BookCategoryToAzureApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookCategoryToAzureApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name== obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+    }
+}
--- BookCategoryToAzureApp/Program.cs
+++ BookCategoryToAzureApp/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
